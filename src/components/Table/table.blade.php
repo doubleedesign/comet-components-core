98b51beff8df25256a7875c2a0f2396cc995506d
Leesa Ward <leesa@doubleedesign.com.au>
2025-02-17 10:23:45 +1100

    Remove some components to be implemented later; Docs: Add intended release and roadmap info

--- src/components/Table/table.blade.php
+++
@@ -1,7 +0,0 @@
-{{-- @var string $tag --}}
-{{-- @var string $classes --}}
-{{-- @var array<string,string> $attributes --}}
-{{-- @var string $content --}}
-<{{ $tag }} @class($classes) @attributes($attributes)>
-	{!! $content !!}
-</{{ $tag }}>
