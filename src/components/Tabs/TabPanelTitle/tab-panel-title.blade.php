eae6f7b37c28949ba854adefd378b8b3ca9c06fe
Leesa Ward <leesa@doubleedesign.com.au>
2025-02-23 14:20:06 +1100

    Bunch of work on Tabs and SiteHeader; WP: Handle blocks not having a direct Comet counterpart

--- src/components/Tabs/TabPanelTitle/tab-panel-title.blade.php
+++ src/components/Tabs/TabPanelTitle/tab-panel-title.blade.php
@@ -1,3 +1,5 @@
-<li @if($classes)@class($classes)@endif @attributes($attributes)>
-	<a href="#{{ $anchor }}" role="tab">{!! $content !!}</a>
+<li role="presentation" @if($classes)@class($classes)@endif @attributes($attributes)>
+	<a role="tab" href="#{{ $anchor }}" aria-controls="{{ $anchor }}">
+		{!! $content !!}
+	</a>
 </li>
