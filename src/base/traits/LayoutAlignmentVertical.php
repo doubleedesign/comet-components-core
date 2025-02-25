4103e84f031ef772181d8a5fa1f4f5dbe63aac00
Leesa Ward <leesa@doubleedesign.com.au>
2025-02-25 13:46:30 +1100

    Fix(Column): Vertical alignment

--- src/base/traits/LayoutAlignmentVertical.php
+++ src/base/traits/LayoutAlignmentVertical.php
@@ -10,7 +10,8 @@ trait LayoutAlignmentVertical {
 	 */
 	protected function set_valign_from_attrs($attributes): void {
 		// In WordPress, some blocks have $attributes['theSetting'] and some have $attributes['layout']['theSetting'] so we need to account for both
-		$vAlign = $attributes['alignItems'] ?? $attributes['layout']['alignItems'] ?? null;
+		// Also different blocks have different attributes for alignment that we need to handle
+		$vAlign = $attributes['alignItems'] ?? $attributes['layout']['alignItems'] ?? $attributes['verticalAlignment'] ?? null;
 		$this->vAlign = isset($vAlign) ? Alignment::fromString($vAlign) : null;
 	}
 }
