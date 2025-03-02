2262f6825eb113ede9c50673423fc85950667e24
Leesa Ward <leesa@doubleedesign.com.au>
2025-03-02 22:00:41 +1100

    Refactor: Remove unused WP Group variations (Row, Stack, Grid)

--- src/components/Stack/stack.blade.php
+++
@@ -1,7 +0,0 @@
-<{{ $tag }} @if ($classes) @class($classes) @endif @attributes($attributes)>
-    @foreach ($children as $child)
-        @if (method_exists($child, 'render'))
-            {{ $child->render() }}
-        @endif
-    @endforeach
-    </{{ $tag }}>
