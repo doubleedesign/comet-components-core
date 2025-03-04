0834e0ce41d348f3e344de6ff6c234a87ff6f993
Leesa Ward <leesa@doubleedesign.com.au>
2025-03-04 12:06:53 +1100

    ImageAndText: Remove orientation options as most use cases should be covered by one setup

--- src/components/ImageAndText/ImageAndText.php
+++ src/components/ImageAndText/ImageAndText.php
@@ -4,23 +4,13 @@ namespace Doubleedesign\Comet\Core;
 #[AllowedTags([Tag::DIV])]
 #[DefaultTag(Tag::DIV)]
 class ImageAndText extends UIComponent {
-	use LayoutOrientation;
-
 	/**
 	 * @var array<ImageWrapper|ContentWrapper> $innerComponents
 	 */
 	protected array $innerComponents;
 
 	function __construct(array $attributes, array $innerComponents) {
-		$this->set_orientation_from_attrs($attributes);
 		parent::__construct($attributes, $innerComponents, 'components.ImageAndText.image-and-text');
-	}
-
-	function get_html_attributes(): array {
-		return array_merge(
-			parent::get_html_attributes(),
-			['data-orientation' => $this->orientation->value]
-		);
 	}
 
 	function render(): void {
