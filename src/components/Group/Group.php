596c37039b252e3af72b4820cfac161da91fc663
Leesa Ward <leesa@doubleedesign.com.au>
2025-03-08 16:44:43 +1100

    Chore: Commit come missed compiled file updates

--- src/components/Group/Group.php
+++ src/components/Group/Group.php
@@ -4,6 +4,12 @@ namespace Doubleedesign\Comet\Core;
 #[AllowedTags([Tag::DIV, Tag::SECTION, Tag::ARTICLE, Tag::ASIDE])]
 #[DefaultTag(Tag::DIV)]
 class Group extends LayoutComponent {
+	/**
+	 * @var ?array $classes
+	 * @supported-values group, group--breakout
+	 */
+	protected ?array $classes;
+
 	function __construct(array $attributes, array $innerComponents) {
 		parent::__construct($attributes, $innerComponents, 'components.Group.group');
 		// Allow something other than "group" to be used as the shortName, primarily for automatic BEM class naming
