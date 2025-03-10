3446213b869bc84efa21d566a022fc7e67184018
Leesa Ward <leesa@doubleedesign.com.au>
2025-03-10 20:56:34 +1100

    Chore: Remove freeform block stuff because it's not currently used

--- src/components/Freeform/freeform.blade.php
+++
@@ -1,4 +0,0 @@
-<div @if ($classes) @class($classes) @endif @attributes($attributes)>
-    <!-- TODO Check if this is the right way to pass the raw HTML for security (letting Blade sanitise rather than preprocessing) -->
-    {{ $content }}
-</div>
