2446b9e98eb3761d60d1bd53eef61d1ba9a6ad63
Leesa Ward <leesa@doubleedesign.com.au>
2025-04-10 23:38:13 +1000

    Refactor: Simplify Group

--- src/components/Group/Group.php
+++ src/components/Group/Group.php
@@ -3,7 +3,9 @@ namespace Doubleedesign\Comet\Core;
 
 #[AllowedTags([Tag::DIV, Tag::SECTION, Tag::ARTICLE, Tag::ASIDE])]
 #[DefaultTag(Tag::DIV)]
-class Group extends LayoutComponent {
+class Group extends UIComponent {
+	use BackgroundColor;
+
 	/**
 	 * @var ?array<string> $classes
 	 * @supported-values group, group--breakout
@@ -12,7 +14,32 @@ class Group extends LayoutComponent {
 
 	function __construct(array $attributes, array $innerComponents) {
 		parent::__construct($attributes, $innerComponents, 'components.Group.group');
+		$this->set_background_color_from_attrs($attributes);
 		// Allow something other than "group" to be used as the shortName, primarily for automatic BEM class naming
 		$this->shortName = $attributes['shortName'] ?? $this->shortName;
+	}
+
+	protected function get_html_attributes(): array {
+		$attributes = parent::get_html_attributes();
+
+		if(isset($this->backgroundColor)) {
+			$attributes['data-background'] = $this->backgroundColor->value;
+		}
+		else if(isset($this->gradient)) {
+			$attributes['data-background'] = 'gradient-' . $this->gradient;
+		}
+
+		return $attributes;
+	}
+
+	public function render(): void {
+		$blade = BladeService::getInstance();
+
+		echo $blade->make($this->bladeFile, [
+			'tag'        => $this->tagName->value,
+			'classes'    => $this->get_filtered_classes_string(),
+			'attributes' => $this->get_html_attributes(),
+			'children'   => $this->innerComponents
+		])->render();
 	}
 }
--- src/components/Group/__tests__/group.php
+++ src/components/Group/__tests__/group.php
@@ -3,7 +3,7 @@ use Doubleedesign\Comet\Core\Group;
 use Doubleedesign\Comet\Core\{Paragraph};
 
 // Attribute keys from component JSON definition
-$attributeKeys = ['backgroundColor', 'classes', 'hAlign', 'tagName', 'testId', 'vAlign'];
+$attributeKeys = ['backgroundColor', 'classes', 'tagName', 'testId'];
 // Filter the request query vars to only those matching the above
 $attributes = array_filter($_REQUEST, fn($key) => in_array($key, $attributeKeys), ARRAY_FILTER_USE_KEY);
 
