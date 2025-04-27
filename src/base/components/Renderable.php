fdd22469832971664db4bce8d28d37cad853dc84
Leesa Ward <leesa@doubleedesign.com.au>
2025-04-27 16:05:24 +1000

    Docs: Update description of context field

--- src/base/components/Renderable.php
+++ src/base/components/Renderable.php
@@ -31,7 +31,7 @@ abstract class Renderable {
 	protected ?array $style = null;
 	/**
 	 * @var ?string $context
-	 * @description The kebab-case or BEM name of the parent component or variant if contextually relevant
+	 * @description The kebab-case or BEM name of the parent component or variant if contextually relevant. May be automatically set by parent component(s).
 	 */
 	protected ?string $context = null;
 	/**
--- src/components/Link/link.blade.php
+++ src/components/Link/link.blade.php
@@ -1,6 +1,6 @@
 <a @if ($classes) @class($classes) @endif @attributes($attributes)>
-    @if ($icon)
-        <i class="{{ $iconPrefix }} {{ $icon }}"></i>
-    @endif
-    {!! $content !!}
+	@if ($icon)
+		<i class="{{ $iconPrefix }} {{ $icon }}"></i>
+	@endif
+	<span>{!! $content !!}</span>
 </a>
