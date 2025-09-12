828fb26ceda05b346c14f5eb3da5fede5169ad6a
Leesa Ward <leesa@doubleedesign.com.au>
2025-09-12 21:58:57 +1000

    Refactor: Create ImageCropProperties trait
    Used in ContentImageAdvanced and ready for upcoming banner variation

---
+++ src/base/traits/ImageCropProperties.php
@@ -0,0 +1,81 @@
+<?php
+namespace Doubleedesign\Comet\Core;
+
+/**
+ * TODO: Image property refinements
+ * Do we really need focal point *and* offset - how would it be handled if both were set but didn't actually work together?
+ * How to handle focal point/offset if there is no aspect ratio set - they wouldn't do anything out of the box
+ * That case should probably be treated as a ContentImageBasic instead - how to enforce that?
+ */
+trait ImageCropProperties {
+    /**
+     * @var AspectRatio|null $aspectRatio
+     * @description Crop banner image to the given aspect ratio
+     */
+    protected ?AspectRatio $aspectRatio = null;
+
+    /**
+     * @var array{x: int, y:int}|null $focalPoint
+     * @description The focal point of the image to use when cropping - x and y values between 0 and 100
+     */
+    protected ?array $focalPoint = ['x' => 50, 'y' => 50];
+
+    /**
+     * @var array{x: int, y:int}|null $offset
+     * @description The percentage offsets of the image to use when cropping
+     */
+    protected ?array $offset = ['x' => 0, 'y' => 0];
+
+    protected function set_aspect_ratio_from_attrs(array $attributes, AspectRatio $default): void {
+        $this->aspectRatio = AspectRatio::tryFrom($attributes['aspectRatio'] ?? '') ?? $default;
+    }
+
+    protected function set_focal_point_from_attrs(array $attributes): void {
+        $field = $attributes['focalPoint'];
+
+        if (is_array($field) && isset($field['x']) && isset($field['y'])) {
+            $this->focalPoint = [
+                'x' => max(0, min(100, (int)$field['x'])),
+                'y' => max(0, min(100, (int)$field['y'])),
+            ];
+        }
+    }
+
+    protected function set_image_offset_from_attrs(array $attributes): void {
+        $field = $attributes['offset'] ?? $attributes['imageOffset'];
+
+        if (is_array($field) && isset($field['x']) && isset($field['y'])) {
+            $this->offset = [
+                'x' => max(0, min(100, (int)$field['x'])),
+                'y' => max(0, min(100, (int)$field['y'])),
+            ];
+        }
+    }
+
+    /**
+     * Build a string to be used in the style attribute
+     * for local CSS properties that can be used to control image cropping
+     *
+     * @return string
+     */
+    public function get_local_css_properties(): string {
+        $properties = [];
+
+        if ($this->focalPoint) {
+            $properties['--focal-point-x'] = $this->focalPoint['x'] . '%';
+            $properties['--focal-point-y'] = $this->focalPoint['y'] . '%';
+        }
+        if ($this->offset) {
+            $properties['--offset-x'] = $this->offset['x'] . '%';
+            $properties['--offset-y'] = $this->offset['y'] . '%';
+        }
+
+        // Return as a string to use in the style attribute
+        return implode(';', array_map(
+            fn($key, $value) => "$key:$value;",
+            array_keys($properties),
+            $properties
+        ));
+    }
+
+}
--- src/components/ContentImageAdvanced/ContentImageAdvanced.php
+++ src/components/ContentImageAdvanced/ContentImageAdvanced.php
@@ -11,70 +11,16 @@ namespace Doubleedesign\Comet\Core;
 #[AllowedTags([Tag::FIGURE, Tag::DIV])]
 #[DefaultTag(Tag::FIGURE)]
 class ContentImageAdvanced extends ContentImageComponent {
-    /**
-     * TODO: Image property refinements
-     * Do we really need focal point *and* offset - how would it be handled if both were set but didn't actually work together?
-     * How to handle focal point/offset if there is no aspect ratio set - they wouldn't do anything out of the box
-     * That case should probably be treated as a ContentImageBasic instead - how to enforce that?
-     */
+    use ImageCropProperties;
 
     // Note: To simplify the layout calculations, this component does not support wrapping the image in a link.
 
-    /**
-     * @var array{x: int, y:int}|null $focalPoint
-     * @description The focal point of the image to use when cropping - x and y values between 0 and 100
-     */
-    protected ?array $focalPoint = null;
-
-    /**
-     * @var array{x: int, y:int}|null $offset
-     * @description The percentage offsets of the image to use when cropping
-     */
-    protected ?array $offset = null;
-
     public function __construct(array $attributes) {
-        $this->focalPoint = $attributes['focalPoint'] ?? null;
+        $this->set_focal_point_from_attrs($attributes);
         $this->set_image_offset_from_attrs($attributes);
-        $this->aspectRatio = AspectRatio::tryFrom($attributes['aspectRatio']) ?? $this->aspectRatio;
+        $this->set_aspect_ratio_from_attrs($attributes, AspectRatio::STANDARD);
         parent::__construct($attributes, 'components.ContentImageAdvanced.content-image-advanced');
         $this->shortName = 'image';
-    }
-
-    private function set_image_offset_from_attrs(array $attributes): void {
-        $field = $attributes['offset'] ?? $attributes['imageOffset'];
-        if (!$field) {
-            $this->offset = ['x' => 0, 'y' => 0];
-        }
-
-        if (is_array($field) && isset($field['x']) && isset($field['y'])) {
-            $this->offset = [
-                'x' => max(0, min(100, (int)$field['x'])),
-                'y' => max(0, min(100, (int)$field['y'])),
-            ];
-        }
-        else {
-            $this->offset = ['x' => 0, 'y' => 0];
-        }
-    }
-
-    public function get_local_css_properties() {
-        $properties = [];
-
-        if ($this->focalPoint) {
-            $properties['--focal-point-x'] = $this->focalPoint['x'] . '%';
-            $properties['--focal-point-y'] = $this->focalPoint['y'] . '%';
-        }
-        if ($this->offset) {
-            $properties['--offset-x'] = $this->offset['x'] . '%';
-            $properties['--offset-y'] = $this->offset['y'] . '%';
-        }
-
-        // Return as a string to use in the style attribute
-        return implode(';', array_map(
-            fn($key, $value) => "$key:$value;",
-            array_keys($properties),
-            $properties
-        ));
     }
 
     protected function get_bem_name(): string {
