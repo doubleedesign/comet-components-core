be39489aecee911ccf06d4f3f72ea050e3358244
Leesa Ward <leesa@doubleedesign.com.au>
2025-09-13 16:42:49 +1000

    Cleanup: CTA and PageHeader can now both use the Container Blade template

--- src/components/CallToAction/call-to-action.blade.php
+++
@@ -1,7 +0,0 @@
-<{{ $tag }} @if ($outerClasses) @class($outerClasses) @endif @attributes($attributes)>
-	@foreach ($children as $child)
-		@if (gettype($child) === 'object' && method_exists($child, 'render'))
-			{{ $child->render() }}
-		@endif
-	@endforeach
-	</{{ $tag }}>
--- src/components/PageHeader/page-header.blade.php
+++
@@ -1,9 +0,0 @@
-<header @if ($outerClasses) @class($outerClasses) @endif @attributes($attributes)>
-	<div @if ($classes) @class($classes) @endif @attributes($innerAttributes)>
-		@foreach ($children as $child)
-			@if (gettype($child) === 'object' && method_exists($child, 'render'))
-				{{ $child->render() }}
-			@endif
-		@endforeach
-	</div>
-</header>
