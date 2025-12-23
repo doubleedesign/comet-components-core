c78ccc520b58f76d8f9fe353ec38afaabfc64ffa
Leesa Ward <leesa@doubleedesign.com.au>
2025-12-23 18:59:17 +1100

    Test(Utils): Add some more unit tests

--- src/base/__tests__/UtilsTest.php
+++ src/base/__tests__/UtilsTest.php
@@ -1,6 +1,74 @@
 <?php
 use Doubleedesign\Comet\Core\Utils;
 
+describe('kebab_case', function() {
+
+    test('converts camelCase to kebab-case', function() {
+        $input = 'purpleMonkeyDishwasher';
+        $result = Utils::kebab_case($input);
+
+        expect($result)->toBe('purple-monkey-dishwasher');
+    });
+
+    test('converts PascalCase to kebab-case', function() {
+        $input = 'PurpleMonkeyDishwasher';
+        $result = Utils::kebab_case($input);
+
+        expect($result)->toBe('purple-monkey-dishwasher');
+    });
+
+    test('handles single word input', function() {
+        $input = 'Purple';
+        $result = Utils::kebab_case($input);
+
+        expect($result)->toBe('purple');
+    });
+
+    test('handles empty string input', function() {
+        $input = '';
+        $result = Utils::kebab_case($input);
+
+        expect($result)->toBe('');
+    });
+
+    test('handles strings with no uppercase letters', function() {
+        $input = 'purplemonkeydishwasher';
+        $result = Utils::kebab_case($input);
+
+        expect($result)->toBe('purplemonkeydishwasher');
+    });
+});
+
+describe('pascal_case', function() {
+    test('converts kebab-case to PascalCase', function() {
+        $input = 'purple-monkey-dishwasher';
+        $result = Utils::pascal_case($input);
+
+        expect($result)->toBe('PurpleMonkeyDishwasher');
+    });
+
+    test('handles single word input', function() {
+        $input = 'purple';
+        $result = Utils::pascal_case($input);
+
+        expect($result)->toBe('Purple');
+    });
+
+    test('handles empty string input', function() {
+        $input = '';
+        $result = Utils::pascal_case($input);
+
+        expect($result)->toBe('');
+    });
+
+    test('handles strings with no hyphens', function() {
+        $input = 'purplemonkeydishwasher';
+        $result = Utils::pascal_case($input);
+
+        expect($result)->toBe('Purplemonkeydishwasher');
+    });
+});
+
 describe('array_diff_end', function() {
 
     test('if nothing matches, the entire main array is returned', function() {
