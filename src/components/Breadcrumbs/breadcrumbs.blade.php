b571565ba09bf5b54ffc779e03098ac11f9ef15c
Leesa Ward <leesa@doubleedesign.com.au>
2026-02-02 10:36:05 +1100

    Fix(Breadcrumbs): More descriptive aria label

--- src/components/Breadcrumbs/breadcrumbs.blade.php
+++ src/components/Breadcrumbs/breadcrumbs.blade.php
@@ -1,4 +1,4 @@
-<nav aria-label="Breadcrumbs" @class($classes) @attributes($attributes)>
+<nav aria-label="Breadcrumb navigation" @class($classes) @attributes($attributes)>
 	<blade-fragment>
 		@include('components._blade-partials.children')
 	</blade-fragment>
