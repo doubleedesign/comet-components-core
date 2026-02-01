c952d142e68df6550c07ca49612cf3baaf61b09d
Leesa Ward <leesa@doubleedesign.com.au>
2026-02-02 10:39:44 +1100

    Feat(CardList): Option for link e.g., view all

--- src/components/CardList/CardList.php
+++ src/components/CardList/CardList.php
@@ -14,14 +14,22 @@ class CardList extends WrappedLayoutComponent {
     protected ?string $heading;
 
     /**
+     * @var array|null $link
+     * @description Optional link to display as a call-to-action, such as "View all". Placement of the link is determined by the horizontal alignment setting.
+     */
+    protected ?array $link;
+
+    /**
      * @var array<Card> $innerComponents
      * @description Cards to display in the list.
      */
     protected array $innerComponents = [];
 
     public function __construct(array $attributes, array $innerComponents) {
-        $headingComponent = isset($attributes['heading']) ? new Heading([], $attributes['heading']) : null;
         $this->set_group_layout_from_attrs($attributes, GroupLayout::GRID);
+        $this->set_layout_alignment_from_attrs($attributes, Alignment::START);
+        $headingComponent = isset($attributes['heading']) ? new Heading([], $attributes['heading']) : null;
+        $linkComponent = isset($attributes['link']) ? new Button($attributes['link'], $attributes['link']['title'] ?? 'View more') : null;
 
         // Add a wrapper to each Card so that it can use container queries based on that
         $updatedInnerComponents = array_map(function($component) {
@@ -35,19 +43,34 @@ class CardList extends WrappedLayoutComponent {
             'data-group-layout'            => $this->layout->value
         ];
 
-        // TODO: This is repetitive, e.g. LinkGroup also does this (it just has a different default)
+        // TODO: This is repetitive, e.g. LinkGroup also does this (it just has a different default, Gallery also uses grid layout...)
         if ($this->layout === GroupLayout::GRID) {
             $groupAttrs['data-max-per-row'] = $this->maxPerRow;
         }
 
-        // And a wrapper around the whole group to separate it from the heading
+        // And a wrapper around the whole card group to separate it from the header and footer
         $wrappedCards = (new Group($groupAttrs, $updatedInnerComponents))->set_bem_element('list');
 
-        parent::__construct(
-            $attributes,
-            $headingComponent ? [$headingComponent, $wrappedCards] : [$wrappedCards],
-            'components.CardList.card-list'
+        $header = new Group(
+            ['context' => 'card-list', 'shortName' => 'header'],
+            [$headingComponent]
         );
+        if ($linkComponent !== null & $this->hAlign === Alignment::START) {
+            $header = new Group(
+                ['context' => 'card-list', 'shortName' => 'header'],
+                [$headingComponent, $linkComponent]
+            );
+        }
+
+        $footer = null;
+        if ($linkComponent !== null && $this->hAlign !== Alignment::START) {
+            $footer = new Group(
+                ['context' => 'card-list', 'shortName' => 'footer'],
+                [$linkComponent]
+            );
+        }
+
+        parent::__construct($attributes, array_filter([$header, $wrappedCards, $footer]), 'components.CardList.card-list');
 
         if ($this->get_is_nested()) {
             $this->tagName = Tag::DIV;
@@ -60,7 +83,6 @@ class CardList extends WrappedLayoutComponent {
         // Always include card-list after any custom context/shortName classes,
         // so we don't end up with .card-list__list with no parent .card-list,
         // and so that styling applies correctly to all card lists regardless of their contextual name
-        // FIXME: It's not actually applying to the outer wrapper, but adding get_filtered_classes() to render_with_wrapper causes duplication
         return array_unique(array_merge($classes, ['card-list']));
     }
 }
