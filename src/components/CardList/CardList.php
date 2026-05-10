b40c68f9e4134564ab3cbf5c99bbefed5771800d
Leesa Ward <leesa@doubleedesign.com.au>
2026-05-10 23:22:27 +1000

    Refactor(EventList,CardList): Consolidate and simplify

--- src/components/CardList/CardList.php
+++ src/components/CardList/CardList.php
@@ -30,22 +30,16 @@ class CardList extends LayoutComponent {
         $this->set_group_layout($attributes);
         $this->set_layout_alignment($attributes, Alignment::START);
         $this->set_is_nested($attributes['isNested'] ?? false);
+        $this->set_color_theme($attributes);
         $headingComponent = isset($attributes['heading']) ? new Heading([], $attributes['heading']) : null;
         $linkComponent = isset($attributes['link']) ? new Button($attributes['link'], $attributes['link']['title'] ?? 'View more') : null;
 
         // Add a wrapper to each Card so that it can use container queries based on that
         $updatedInnerComponents = array_map(function($component) {
-            return new Group(['context' => 'card-list__list', 'shortName'  => 'item'], [$component]);
+            return new Group(['shortName' => 'item'], [$component]);
         }, $innerComponents);
 
-        $groupAttrs = [
-            'colorTheme'                   => $this->colorTheme->value ?? 'primary',
-            'shortName'                    => 'card-list',
-            'role'                         => 'group',
-            'data-group-layout'            => $this->layout->value
-        ];
-
-        // TODO: This is repetitive, e.g. LinkGroup also does this (it just has a different default, Gallery also uses grid layout...)
+        $groupAttrs = $this->get_inner_group_attributes();
         if ($this->layout === GroupLayout::GRID) {
             $groupAttrs['data-max-per-row'] = $this->maxPerRow;
         }
@@ -54,12 +48,12 @@ class CardList extends LayoutComponent {
         $wrappedCards = (new Group($groupAttrs, $updatedInnerComponents))->set_bem_element('list');
 
         $header = new Group(
-            ['context' => 'card-list', 'shortName' => 'header'],
+            ['shortName' => 'header'],
             [$headingComponent]
         );
         if ($linkComponent !== null & $this->hAlign === Alignment::START) {
             $header = new Group(
-                ['context' => 'card-list', 'shortName' => 'header'],
+                ['shortName' => 'header'],
                 [$headingComponent, $linkComponent]
             );
         }
@@ -67,24 +61,37 @@ class CardList extends LayoutComponent {
         $footer = null;
         if ($linkComponent !== null && $this->hAlign !== Alignment::START) {
             $footer = new Group(
-                ['context' => 'card-list', 'shortName' => 'footer'],
+                ['shortName' => 'footer'],
                 [$linkComponent]
             );
         }
 
-        parent::__construct($attributes, array_filter([$header, $wrappedCards, $footer]), 'components.CardList.card-list');
+        parent::__construct(
+            $attributes,
+            array_filter([$header, $wrappedCards, $footer]),
+            'components.CardList.card-list'
+        );
 
         if ($this->get_is_nested()) {
             $this->tagName = Tag::DIV;
         }
     }
 
-    public function get_filtered_classes(): array {
-        $classes = parent::get_filtered_classes();
+    protected function get_inner_group_attributes(): array {
+        return array(
+            'shortName'                    => 'card-list',
+            'role'                         => 'group',
+            'data-group-layout'            => $this->layout->value
+        );
+    }
 
-        // Always include card-list after any custom context/shortName classes,
-        // so we don't end up with .card-list__list with no parent .card-list,
-        // and so that styling applies correctly to all card lists regardless of their contextual name
-        return array_unique(array_merge($classes, ['card-list']));
+    protected function get_html_attributes(): array {
+        $attributes = parent::get_html_attributes();
+
+        if ($this->colorTheme) {
+            $attributes['data-color-theme'] = $this->colorTheme->value;
+        }
+
+        return $attributes;
     }
 }
--- src/components/EventList/EventList.php
+++ src/components/EventList/EventList.php
@@ -10,70 +10,21 @@ namespace Doubleedesign\Comet\Core;
  */
 #[AllowedTags([Tag::DIV, Tag::SECTION])]
 #[DefaultTag(Tag::SECTION)]
-class EventList extends LayoutComponent {
-    use ColorTheme;
-    use GroupLayoutType;
-    use NestedState;
-
-    /**
-     * @var ?string $heading
-     * @description The heading for the event list.
-     */
-    protected ?string $heading;
-
-    /**
-     * @var array<EventCard> $innerComponents
-     * @description Event cards to display.
-     */
-    protected array $innerComponents;
-
-    /**
-     * Link to a page to view all events; include only if this is not already the "all events" context.
-     *
-     * @var string|null
-     */
-    protected ?string $viewAllUrl;
+class EventList extends CardList {
 
     public function __construct(array $attributes, array $innerComponents) {
         $attributes['shortName'] = $attributes['shortName'] ?? 'events';
-        $this->bladeFile = $bladeFile ?? 'components.EventList.event-list';
-        $this->set_group_layout($attributes);
-        $this->set_is_nested($attributes['isNested'] ?? false);
-        $this->set_color_theme($attributes);
-        $this->viewAllUrl = $attributes['viewAllUrl'] ?? null;
-
-        $groupAttrs = [
-            'tagName'                      => 'ul',
-            'context'                      => 'events',
-            'shortName'                    => 'list',
-            'colorTheme'                   => $this->colorTheme->value ?? 'primary',
-            'data-group-layout'            => $this->layout->value
+        $attributes['link'] = [
+            'href' => $attributes['viewAllUrl'] ?? null
         ];
 
-        // TODO: This is repetitive, CardList and LinkGroup also do this (one just has a different default)
-        if ($this->layout === GroupLayout::GRID) {
-            $groupAttrs['data-max-per-row'] = $this->maxPerRow;
-        }
+        parent::__construct($attributes, $innerComponents);
+    }
 
-        $wrappedCards = (new Group($groupAttrs, $innerComponents));
-
-        $headingComponent = $attributes['heading'] ? new Heading(['context' => 'events'], $attributes['heading']) : null;
-        $linkComponent = $this->viewAllUrl ? new Button(['classes' => ['button--view-all'], 'href' => $this->viewAllUrl], 'View all') : null; // TODO: Make the label configurable
-
-        // Call constructor so BEM context gets initialised
-        parent::__construct(
-            $attributes,
-            array_filter([$headingComponent, $wrappedCards, $linkComponent]),
-            'components.EventList.event-list'
+    protected function get_inner_group_attributes(): array {
+        return array_merge(
+            parent::get_inner_group_attributes(),
+            ['tagName'  => 'ul']
         );
-
-        // ...then apply it to the cards
-        $wrappedCards->innerComponents = array_map(function($card) use ($wrappedCards) {
-            if ($card instanceof EventCard && !$card->get_context()) {
-                $card->update_context($wrappedCards->get_bem_prefix());
-            }
-
-            return $card;
-        }, $wrappedCards->innerComponents);
     }
 }
